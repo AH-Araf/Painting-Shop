8b690f26b538159e7afa672fc14353a61043eaed
Araf <arafathossain.baiust@gmail.com>
2022-12-22 15:49:32 +0600

    Done

--- src/Pages/FollowUs.js
+++ src/Pages/FollowUs.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import t from '././../assets/images/t.png';
 import u from '././../assets/images/u.png';
+import v from '././../assets/images/v.png';
+import w from '././../assets/images/w.png';
 
 const FollowUs = () => {
     return (
@@ -10,11 +13,30 @@ const FollowUs = () => {
                 <p>Social media for latest offers & monthly contests</p>
             </div>
             <div className='d-flex justify-content-center'>
-                <div className='image-size'>
-                    <img src={t} alt="" />
-                </div>
-                <div className='image-size'>
-                    <img src={u} alt="" />
+                
+                <a target="_blank" href='https://www.facebook.com/buttistore'>
+                    <div className='image-size'>
+                        <img src={t} alt="" />
+                    </div>
+                </a>
+
+                <a target="_blank" href='https://www.instagram.com/buttistore/'>
+                    <div className='image-size'>
+                        <img src={u} alt="" />
+                    </div>
+                </a>
+            </div>
+            <div className='title mb-5'>
+                <h1>Download the app now</h1>
+                <img src={v} alt="" className='image-only'/>
+                <a href="https://play.google.com/store/apps/details?id=com.app.buttistore&hl=en_IN&gl=US"><button type="button" class="btn btn-outline-primary">Download Now</button></a>
+            </div>
+
+            <div>
+                <div className='center pt-5'><img src={w} alt="" /></div>
+                <div className='title'>
+                    <h1>Super-express</h1>
+                    <p>Get your dream picture home delivered within 3 days. This service is available in select cities only.</p>
                 </div>
             </div>
         </div>
--- src/Pages/Footer.js
+++ src/Pages/Footer.js
@@ -1,9 +1,39 @@
 import React from 'react';
+import { FaWhatsapp, FaFacebook,FaInstagram,FaTwitter } from 'react-icons/fa';
 
 const Footer = () => {
     return (
-        <div>
-            <h1>Footer</h1>
+        <div className='footer-container p-5'>
+            <div className='d-flex justify-content-around'>
+                <div>
+                    <h4>SITE MAP</h4>
+                    <p>Home</p>
+                    <p>Blog</p>
+                    <p>Terms & Conditions</p>
+                    <p>Privacy Policy</p>
+                    <p className='mt-5 text-danger'>+91 7795117716</p>
+                    <p className='text-info'>info@buttistore.com</p>
+                </div>
+                <div>
+                    <h4>PRODUCTS</h4>
+                    <p>Digi Painting</p>
+                    <p>Digi Merge Painting</p>
+                    <p>Old Picture Digi Painting</p>
+                    <p>Handmade Painting</p>
+                    <p>Handmade Pencil Sketch</p>
+                </div>
+                <div>
+                    <h4>Join Our Family</h4>
+                    <p>Subscribe to our newsletter and be among the first to hear about new deals and special offers.</p>
+                    <h5 className='mt-5'>Follow Us</h5>
+                    <div className='d-flex'>
+                        <a href="https://api.whatsapp.com/send/?phone=917795117716&text&type=phone_number&app_absent=0" target="_blank"><h3><FaWhatsapp/></h3></a>
+                        <a href="https://www.facebook.com/buttistore" target="_blank"><h3 className='ms-3'><FaFacebook/></h3></a>
+                        <a href="https://www.instagram.com/buttistore/" target="_blank"><h3 className='ms-3'><FaInstagram/></h3></a>
+                        <a href="https://twitter.com/buttistore" target="_blank"><h3 className='ms-3'><FaTwitter/></h3></a>
+                    </div>
+                </div >
+            </div>
         </div>
     );
 };
